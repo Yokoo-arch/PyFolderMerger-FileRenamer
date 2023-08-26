583f3f98ecc43fa778322270b26f03e191ec8220
Yokoo-arch <ambroise.laux@gmail.com>
2023-08-26 23:45:10 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,5 @@
+"""
+Wrote by Yokoo-arch 2023 (https://github.com/Yokoo-arch).
+Github repository: https://github.com/Yokoo-arch/***.
+If you have any issues, please feel free to open an issue on the Github repository.
+"""
